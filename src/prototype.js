e3f61b09989bc53999e163dda77b5d6a3fd65ca8
Jared Grippe <jared@deadlyicon.com>
2009-07-21 11:39:49 -0700

    added github url to dist header

--- src/prototype.js
+++ src/prototype.js
@@ -1,6 +1,8 @@
 /*  Prototype JavaScript framework, version <%= PROTOTYPE_VERSION %>
  *  (c) 2005-2009 Sam Stephenson
+ *
  *  Extended by Jared Grippe jared@jaredgrippe.com
+ *  http://github.com/deadlyicon/prototype
  *
  *  Prototype is freely distributable under the terms of an MIT-style license.
  *  For details, see the Prototype web site: http://www.prototypejs.org/
