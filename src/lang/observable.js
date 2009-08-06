dad2ed8e091e0e79faf92f6a717f22a2b516f6a0
Jared Grippe <jared@deadlyicon.com>
2009-08-06 16:08:05 -0700

    optomized Observable.fire and added Observable.observeOnce also updated some documentation

--- src/lang/observable.js
+++ src/lang/observable.js
@@ -7,10 +7,10 @@
  *  `Enumerable` is a _mixin_: a set of methods intended not for standaone
  *  use, but for incorporation into other objects.
  *
- *      var myClass = Class.create(Observable,{..})
- *      new myClass()
- *        .observe('born',function(event){..})
- *        .fire('born',memo);
+ *      var Frog = Class.create(Observable);
+ *      var f = new Frog;
+ *      f.observe('jump', function(frog, height){ ... });
+ *      f.fire('jump', 14.3);
  **/
 var Observable = (function() {
 
@@ -28,6 +28,20 @@ var Observable = (function() {
   }
 
   /**
+   *  object.observeOnce(eventName, handler) -> Element
+   *
+   *  Registers an event handler that only executes once
+  **/
+  function observeOnce(eventName, handler){
+    var __observer = function selfStoppingObserver(){
+      this.stopObserving(eventName, __observer);
+      return handler.apply(this, arguments);
+    }.bind(this);
+    this.observe(eventName, __observer);
+    return this;
+  }
+
+  /**
    *  object.stopObserve(eventName, handler) -> Element
    *
    *  UnRegisters an event handler onto a javascript object
@@ -41,25 +55,30 @@ var Observable = (function() {
   }
 
   /**
-   *  object.fire(element, eventName[, memo]) -> Event
-   *  - memo (?): Metadata for the event. Will be accessible through the
-   *    event's `memo` property.
+   *  object.fire(eventName[, memo]) -> Event
+   *  - memo (?): Metadata for the event. Will be passed to the observer
+   *    along with a reference to `this`
    *
-   *  Fires a custom event of name `eventName` with `element` as its target.
+   *  Fires a custom event of name `eventName` with `object` as its target.
+   *
+
   **/
-  function fire(eventName){
-    var memo = $A(arguments), eventName = memo.shift();
+  function fire(eventName, memo){
+    var memo = $A(arguments), eventName = memo.shift(), handlers;
     memo.unshift(this);
-    returning(this, function(){
-      if (!this._eventHandlers || !this._eventHandlers[eventName]) return;
-      this._eventHandlers[eventName].each(function(handler) {
-        if (Object.isFunction(handler)) handler.apply(this, memo);
-      },this);
-    });
+    if (this._eventHandlers && (handlers = this._eventHandlers[eventName]))
+      for (var i=0; i < handlers.length; i++)
+        if (Object.isFunction(handlers[i]))
+          try{ handlers[i].apply(this, memo); }
+          catch(e){ (function() { throw e; }).defer(); }
+    return this;
   }
+
 
   return {
     observe:       observe,
+    observeOnce:   observeOnce,
+    observeNext:   observeOnce,
     stopObserving: stopObserving,
     fire:          fire
   };
